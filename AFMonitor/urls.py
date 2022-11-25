520aa3f3529908f17e51231d4fc2bf46dd1ed427
Jonah <jonahjones9224@gmail.com>
2022-11-24 23:46:55 -0500

    Merged all to Index, did basic formating

--- AFMonitor/urls.py
+++ AFMonitor/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'AFMonitor'
 urlpatterns = [
     path("", views.home, name="home"),
-    path("arrivals", views.arrivals, name="arrivals"),
-    path("departures", views.departures, name="departures"),
-    path("weather", views.weather, name="weather"),
+    #path("arrivals", views.arrivals, name="arrivals"),
+    #path("departures", views.departures, name="departures"),
+    #path("weather", views.weather, name="weather"),
 ]
--- AFMonitor/views.py
+++ AFMonitor/views.py
@@ -4,12 +4,30 @@ from bs4 import BeautifulSoup
 from django.shortcuts import render
 
 def home(request):
+    arrivals = farrivals()
+    departures = fdepartures()
+    weather = fweather()
+
     return render(
         request,
-        'AFMonitor/index.html'
+        'AFMonitor/index.html',
+        {
+            'arrivals': arrivals,
+            'departures': departures,
+            'weatherText': weather['WeatherText'],
+            'weatherIcon': weather['WeatherIcon'],
+            'temp': weather['Temperature']['Metric']['Value'],
+            'real': weather['RealFeelTemperature']['Metric']['Value'],
+            'windDegree': weather['Wind']['Direction']['Degrees'],
+            'windSpeed': weather['Wind']['Speed']['Metric']['Value'],
+            'windGust': weather['WindGust']['Speed']['Metric']['Value'],
+            'pressure': weather['Pressure']['Imperial']['Value']
+
+        }
     )
 
-def arrivals(request):
+# def arrivals(request):
+def farrivals():
 
     URL = "https://www.toronto-pearson-airport.com/pearson-arrivals.php"
     array = BeautifulSoup(requests.get(URL).content, "html.parser").find("tbody").find_all("tr", class_="")
@@ -21,15 +39,17 @@ def arrivals(request):
             x = item.text.strip()
             data.append(x)
         flights.append(data)
-    return render(
-        request,
-        'AFMonitor/arrivals.html',
-        {
-            'flights': flights
-        }
-    )
-# Create your views here.
-def departures(request):
+    return flights
+    # return render(
+    #     request,
+    #     'AFMonitor/arrivals.html',
+    #     {
+    #         'flights': flights
+    #     }
+    # )
+
+# def departures(request):
+def fdepartures():
 
     URL = "https://www.toronto-pearson-airport.com/pearson-departures.php"
     array = BeautifulSoup(requests.get(URL).content, "html.parser").find("tbody").find_all("tr", class_="")
@@ -41,15 +61,17 @@ def departures(request):
             x = item.text.strip()
             data.append(x)
         flights.append(data)
-    return render(
-        request,
-        'AFMonitor/departures.html',
-        {
-            'flights': flights
-        }
-    )
+    return flights
+    # return render(
+    #     request,
+    #     'AFMonitor/departures.html',
+    #     {
+    #         'flights': flights
+    #     }
+    # )
 
-def weather(request):
+# def weather(request):
+def fweather():
 
     API="S0sx8BspNrYA2N05NLvVOjnY5MbdRU9V"
     location="Lester B. Pearson International Airport"
@@ -61,19 +83,20 @@ def weather(request):
 
     getforecast="http://dataservice.accuweather.com/currentconditions/v1/"+locationkey+"?apikey="+API+"&details=true"
     with urllib.request.urlopen(getforecast) as getforecast:
-        data=json.loads(getforecast.read().decode())[0]
+        weather=json.loads(getforecast.read().decode())[0]
 
-    return render(
-        request,
-        'AFMonitor/weather.html',
-        {
-            'weatherText': data['WeatherText'],
-            'weatherIcon': data['WeatherIcon'],
-            'temp': data['Temperature']['Metric']['Value'],
-            'real': data['RealFeelTemperature']['Metric']['Value'],
-            'windDegree': data['Wind']['Direction']['Degrees'],
-            'windSpeed': data['Wind']['Speed']['Metric']['Value'],
-            'windGust': data['WindGust']['Speed']['Metric']['Value'],
-            'pressure': data['Pressure']['Imperial']['Value']
-        }
-    )
+    return weather
+    # return render(
+    #     request,
+    #     'AFMonitor/weather.html',
+    #     {
+    #         'weatherText': weather['WeatherText'],
+    #         'weatherIcon': weather['WeatherIcon'],
+    #         'temp': weather['Temperature']['Metric']['Value'],
+    #         'real': weather['RealFeelTemperature']['Metric']['Value'],
+    #         'windDegree': weather['Wind']['Direction']['Degrees'],
+    #         'windSpeed': weather['Wind']['Speed']['Metric']['Value'],
+    #         'windGust': weather['WindGust']['Speed']['Metric']['Value'],
+    #         'pressure': weather['Pressure']['Imperial']['Value']
+    #     }
+    #)
